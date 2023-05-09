9f936d3a6f18bf2a82820a4c640aa3b4257f29d1
JustasMit <44390650+JustasMit@users.noreply.github.com>
2023-05-09 17:04:57 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react"
+import MapView from "@arcgis/core/views/MapView"
+import WebMap from "@arcgis/core/WebMap"
+
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const mapDiv = useRef(null)
+
+	useEffect(() => {
+		if (mapDiv.current) {
+			const webmap = new WebMap({
+				portalItem: {
+					id: "aa1d3f80270146208328cf66d022e09c",
+				},
+			})
+
+			const view = new MapView({
+				container: mapDiv.current,
+				map: webmap,
+			})
+		}
+	}, [])
+
+	return <div className="mapDiv" ref={mapDiv}></div>
 }
 
-export default App;
+export default App
